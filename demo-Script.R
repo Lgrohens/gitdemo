98825697987d74453c38675d0819cef4705c04b7
Lgrohens <55613812+Lgrohens@users.noreply.github.com>
2019-10-03 10:43:08 -0700

    Add files via upload

---
+++ demo-Script.R
@@ -0,0 +1,14 @@
+# ===================================================================
+# Title: Lab 05
+# Description:
+#   This script computes descriptive statistics, as well as 
+#   various exploratory data visualizations.
+# Input(s): data file 'nba2018-players.csv'
+# Output(s): summary data files, and plots
+# Author: Laura Grohens
+# Date: 10-02-2019
+# ===================================================================
+
+# packages
+library(dplyr)    # data wrangling
+library(ggplot2)  # graphics
